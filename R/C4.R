173bee07cdf8c508eba2232c85396c776e470c3a
Bowen Mince <mincebow@grinnell.edu>
2021-01-21 18:40:58 -0600

    Even chapters

---
+++ R/C4.R
@@ -0,0 +1,240 @@
+#' Chapter 4 Bacteria
+#'
+#' Two students, Isaac and Courtney, sampled surfaces around their campus to analyze the prevalence of bacteria.
+#' They compared different types of surfaces in both residential and academic buildings.
+#'
+#' Data were collected by wiping surfaces with a wet Q-tip and swabbing the result on a standard
+#' nutrient agar plate. Locations were all tested on April 24, 2009 over the course of a two-hour period.
+#' The plates were incubated at 37°C for 48 hours before colony-forming units (CFUs) were counted as a
+#' measure of bacteria levels. Figure 4.5 shows several of the plates from this study. When CFUs exceeded
+#' 400 per plate, one fourth of the plate was counted and the total was calculated from that sample.
+#'
+#' Six different buildings were swabbed, with two buildings representing each type of facility:
+#' • Academic buildings: Noyce and ARH
+#' • Public residential buildings: Norris and Dibble
+#' • Private residences: Cowles apartment 7110 and Yellow House (1478 Park St.)
+#' Within each building, two faucets, two door handles, and two desk surfaces were swabbed.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Bacteria
+#' @usage Bacteria
+#' @format A data frame with 36 rows and three columns (Building, Location, and Count)
+"Bacteria"
+
+
+#' Chapter 4 Cholesterol
+#'
+#' Cholesterol is a waxy substance found in blood and cell membranes. All animals need some cholesterol
+#' in their system; however, too much cholesterol can cause heart attacks and strokes. A study was
+#' conducted to determine how exercise, diet, and three types of drugs impact cholesterol levels.
+#' Seventy-two patients at a nearby hospital who had been diagnosed with high cholesterol (a level greater
+#' than 240 milligrams per deciliter) consented to be in the study. Each of the 72 patients was randomly
+#' assigned to a specific treatment, and after six months the patients’ cholesterol levels were measured
+#' again. These measurements are provided in the Cholesterol data set.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Cholesterol
+#' @usage Cholesterol
+#' @format A data frame with 72 rows and five columns (Patient, Exercise, Diet, Drug, and Cholesterol)
+"Cholesterol"
+
+
+#' Chapter 4 Cups
+#'
+#' A manager of a manufacturing company, shift managers, cup machine operators,
+#' and a lone statistician decided to identify which factors were most influential in keeping their cups from leaking.
+#' Over 30 possible factors were identified, but after some thoughtful discussions the group settled on
+#' six variables that should be tested for their effects on leaking cups. One of the six factors of interest
+#' was which paper supplier to use. Since the company was considering changing suppliers, funds were
+#' available to do some product testing before a purchase was made. However, each trial (each run of
+#' production under specified factor conditions) would cost the company thousands of dollars in lost
+#' production time, material costs, and employee costs. T
+#'
+#' The company agreed to conduct 32 tests, but wanted to test all six factors and
+#' all corresponding twoway interactions. Fractional factorial designs are very useful
+#' for this type of exploratory data analysis. The details of fractional factorial designs
+#' are beyond the scope of this text. However, balanced
+#' data are a key concept behind these designs. For example, in the Cups data, every factor has two levels
+#' and each level has 16 observations. In addition, within the first factor level (the 16 observations
+#' where side-seam temperature is set to 70%), every other factor is still balanced (every other factor
+#' has 8 observations at each level).
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Cups
+#' @usage Cups
+#' @format A data frame with 32 rows and seven columns
+"Cups"
+
+
+#' Chapter 4 Cholesterol
+#'
+#' Cholesterol is a waxy substance found in blood and cell membranes. All animals need some cholesterol
+#' in their system; however, too much cholesterol can cause heart attacks and strokes. A study was
+#' conducted to determine how exercise, diet, and three types of drugs impact cholesterol levels.
+#' Seventy-two patients at a nearby hospital who had been diagnosed with high cholesterol (a level greater
+#' than 240 milligrams per deciliter) consented to be in the study. Each of the 72 patients was randomly
+#' assigned to a specific treatment, and after six months the patients’ cholesterol levels were measured
+#' again. These measurements are provided in the Cholesterol data set.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Cholesterol
+#' @usage Cholesterol
+#' @format A data frame with 72 rows and five columns (Patient, Exercise, Diet, Drug, and Cholesterol)
+"Cholesterol"
+
+
+#' Chapter 4 Games2C4
+#'
+#' The data set Games2 shows a column Type2 with four types of games based on distracter and
+#' which hand was used.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Games2C4
+#' @usage Games2C4
+#' @format A data frame with 40 rows and six columns
+"Games2C4"
+
+
+#' Chapter 4 MemoryA
+#'
+#' Michael Eysenck tested 100 subjects (50 people between the ages of 55 and 65, 50 younger people)
+#' to determine if there was a relationship between age and memory.10 Each subject was shown 27
+#' words and asked to recall as many of those words as possible. He also tested whether five different
+#' techniques impacted memory. Each subject was given one of five types of instructions:
+#'   Counting: count the number of letters in each word
+#'   Rhyming: think of a word that rhymes with each word
+#'   Adjective: think of an adjective to describe each word
+#'   Imagery: create an image of each word
+#'   Intentional: remember as many words as possible
+#'
+#' The subjects in the first four groups were not aware that they would later be asked to recall each
+#' word.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name MemoryA
+#' @usage MemoryA
+#' @format A data frame with 100 rows and three columns
+"MemoryA"
+
+
+#' Chapter 4 Movies
+#'
+#' The file Movies contains the ratings and genres of movies that came out in 2008. This is simply an
+#' observational study, as clearly movie producers do not try to create an equal number of G, PG, and R
+#' movies each year.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Movies
+#' @usage Movies
+#' @format A data frame with 210 rows and four columns
+"Movies"
+
+
+#' Chapter 4 PaperTowel2
+#'
+#' Supplementary Data for PaperTowels.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name PaperTowel2
+#' @usage PaperTowel2
+#' @format A data frame with 234 rows and three columns
+"PaperTowel2"
+
+
+#' Chapter 4 PaperTowels
+#'
+#' As a final project in an introductory statistics class, several students decided to conduct a study to test the
+#' strength of paper towels. Several television advertisements had claimed that a certain brand of paper towel was
+#' the strongest, and these students wanted to determine if there really was a difference. The students sampled
+#' 26 towels from two brands of paper towels, Comfort and Decorator.
+#'
+#' Before any data were collected, these students determined that the following conditions should be held as
+#' constant as possible throughout the study:
+#'   • Paper towels were selected that had the same size.
+#'   • The towels were held at all four corners by two people.
+#'   • Weights (10, 25, 50, 100, or 250 grams) were slowly added to the center of each towel by a third person until it broke.
+#' In this study, there are two factors. One has two levels, Comfort (Brand C) or Decorator (Brand D), and
+#' the other has three levels (0, 5, or 15 drops of water applied to the center of the paper towel). This leads to
+#' 2 * 3 = 6 conditions, called factor-level combinations or factorial combinations:
+#'   Brand C and 0 drops of water
+#'   Brand C and 5 drops of water
+#'   Brand C and 15 drops of water
+#'   Brand D and 0 drops of water
+#'   Brand D and 5 drops of water
+#'   Brand D and 15 drops of water
+#' Twenty-six sheets were tested at each of the six factor-level combinations. Thus, there are 156 experimental
+#' units used in this study. The response variable is the breaking strength of each paper towel in grams. Breaking
+#' strength is defined as the total weight that each towel successfully held. The next additional weight caused
+#' the towel to break.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name PaperTowels
+#' @usage PaperTowels
+#' @format A data frame with 156 rows and three columns
+"PaperTowels"
+
+
+#' Chapter 4 Popcorn
+#'
+#' Two popcorn lovers, who also happened to be taking a statistics course, decided to test whether there is a
+#' difference in quality between microwave popcorn brands. Yvonne and Tue wanted to know if a cheaper brand
+#' of popcorn was just as good as more expensive popcorn. These students could have chosen to conduct a study
+#' that could be analyzed with a two-sample t-test if they had simply compared two brands of popcorn. However,
+#' if they did a two-sample t-test, they would need to hold many factors constant, such as the type of microwave,
+#' cooking time, and storage procedures. Since Yvonne and Tue believed that some of these factors could also
+#' impact the quality of the popcorn, they decided to include some of these additional factors in their study.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Popcorn
+#' @usage Popcorn
+#' @format A data frame with 32 rows and five columns
+"Popcorn"
+
+
+#' Chapter 4 Soda
+#'
+#' Two students, Julie and Daphne, conducted an experiment to test the effects of soda type (Pepsi
+#' vs. 7-Up), angle of the cup (cup flat on the table or slightly tipped), and soda temperature (refrigerated
+#' at 5°C vs. room temperature at 21°C) on the height of fizz produced when soda was poured out of a
+#' can into a cup. For each of the 24 trials, these students poured a can of either Pepsi or 7-Up into a clear
+#' cup and measured the peak fizz (in centimeters) produced, using a ruler on the outside of the cup.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Soda
+#' @usage Soda
+#' @format A data frame with 24 rows and four columns
+"Soda"
+
+#' Chapter 4 Variable_DescriptionsC4
+#'
+#' An index of the chapter 4 data included in this package.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Variable_DescriptionsC4
+#' @usage Variable_DescriptionsC4
+#' @format A data frame with 43 rows and four columns
+"Variable_DescriptionsC4"
+
+
+
+
+
+
+
+
+
---
+++ R/C6.R
@@ -0,0 +1,249 @@
+#' Chapter 6 Baboon_Variable_Descriptions
+#'
+#' A codebook for the Baboon data.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Baboon_Variable_Descriptions
+#' @usage Baboon_Variable_Descriptions
+#' @format A data frame with nine rows and four columns
+"Baboon_Variable_Descriptions"
+
+
+#' Chapter 6 Baboon
+#'
+#' Primatologist Vicki Bentley-Condit of Grinnell College studied interactions between female and infant
+#' yellow baboons (Papio cynocephalus cynocephalus) at the Tana River National Primate Reserve in Kenya.
+#' She collected the data for her study by observing baboons in 20-minute focal samples over an 11-month
+#' period in 1991–1992. Her subjects included 23 female baboons, 11 of which were mothers with infants (no
+#' mother with more than one offspring). Bentley-Condit observed and recorded interactions between females
+#' and infants, excluding interactions between a mother and her own offspring.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Baboon
+#' @usage Baboon
+#' @format A data frame with 242 rows and nine columns
+"Baboon"
+
+
+#' Chapter 6 babyweights
+#'
+#' The file BabyWeight provides newborn weights for a simple random sample of 135 infants born in
+#' the United States in 1995.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name babyweights
+#' @usage babyweights
+#' @format A data frame with 135 rows and one column
+"babyweights"
+
+
+#' Chapter 6 Table_6.1
+#'
+#' Table 6.1 shows data from 37 FNA slide samples. Slides with smooth ellipsoid-shaped nuclei were classified
+#' as round, and slides with poorly shaped cell nuclei were classified as concave. A biopsy was also conducted
+#' on each of these samples to determine if each was malignant or benign.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.1
+#' @usage Table_6.1
+#' @format A data frame with two rows and three columns
+"Table_6.1"
+
+
+#' Chapter 6 Table_6.1
+#'
+#' Table 6.1 shows data from 37 FNA slide samples. Slides with smooth ellipsoid-shaped nuclei were classified
+#' as round, and slides with poorly shaped cell nuclei were classified as concave. A biopsy was also conducted
+#' on each of these samples to determine if each was malignant or benign.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.1
+#' @usage Table_6.1
+#' @format A data frame with two rows and three columns
+"Table_6.1"
+
+
+#' Chapter 6 Table_6.10
+#'
+#' At a summer tea party in the late 1920s, a lady claimed that she could determine by taste if tea had
+#' been poured into the milk or if milk had been poured into the tea. Sir Ronald Fisher, well known
+#' for his work in design of experiments, discussed how to test this lady’s claim. The lady was asked
+#' to taste several cups in random order and identify each as tea first or milk first. Although the actual
+#' results were not published, rumor has it that the lady correctly identified every single cup.
+#'
+#' Let’s assume that the lady was given 14 cups of tea in random order. She was told that half the
+#' cups had tea first and the other half had milk first. Table 6.10 provides hypothetical results.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.10
+#' @usage Table_6.10
+#' @format A data frame with three rows and four columns
+"Table_6.10"
+
+
+#' Chapter 6 Table_6.11
+#'
+#' Members of the Donner party attempted a new route between Fort Bridger, Wyoming, and the Humboldt
+#' River, Nevada. This new route took much longer than expected, and the entire party was trapped in the
+#' Sierra Nevada in the winter of 1846–1847. By the time they were rescued in the spring, many members
+#' had died. Table 6.11 lists the gender and survival status of all adults who were trapped during this trip.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.11
+#' @usage Table_6.11
+#' @format A data frame with three rows and four columns
+"Table_6.11"
+
+
+#' Chapter 6 Table_6.12
+#'
+#' Many people with cocaine addictions also suffer from mood disorders like depression or a manicdepressive disorder.
+#' To aid in the process of trying to break a patient’s addiction to cocaine, a
+#' common antidepressant known as lithium is often given to patients in drug treatment centers. A
+#' randomized, comparative experiment was performed to determine whether another commonly
+#' prescribed antidepressant, disipramine, could be used to reduce the likelihood of patients’ using
+#' cocaine again or whether antidepressant therapy helped at all. The 72 patients selected for study
+#' over a 3-year period were randomly assigned to one of three treatments: disipramine, lithium, or
+#' a placebo. After the end of the 6-week treatment period, these patients were interviewed to determine if they
+#' had been successful in stopping their cocaine habit. A success was defined as having
+#' stopped using cocaine for 3 weeks or more during the 6-week treatment period. The data from the
+#' study are shown in Table 6.12.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.12
+#' @usage Table_6.12
+#' @format A data frame with four rows and four columns
+"Table_6.12"
+
+
+#' Chapter 6 Table_6.13
+#'
+#' A local organization conducted a traffic study to determine if traffic collisions were more likely on
+#' Friday and Saturday nights than on other nights. The city council was considering a proposal that
+#' bars in a specific district be closed earlier in an effort to reduce late-night accidents in the area. The
+#' assumption was that a larger number of traffic accidents would correspond to the heaviest bar traffic
+#' (which occurred on the weekends). Table 6.13 shows data collected on traffic accidents that had
+#' occurred in that district over the past two years.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.13
+#' @usage Table_6.13
+#' @format A data frame with one row and eight columns
+"Table_6.13"
+
+
+#' Chapter 6 Table_6.14
+#'
+#' The extended activities in Section 6.7 discussed a clinical study on Zocor, a drug used in lowering
+#' cholesterol, to determine if there was a difference between the treatment (Zocor) and the placebo in
+#' reducing the number of deaths from heart attacks. Table 6.14 shows the original data from this study.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.14
+#' @usage Table_6.14
+#' @format A data frame with three rows and four columns
+"Table_6.14"
+
+
+#' Chapter 6 Table_6.15
+#'
+#' In October 1995, the United Kingdom Committee on Safety of Medicines (CSM) issued a warning
+#' to 190,000 general practitioners, pharmacists, and directors of public health about oral contraceptive
+#' pills containing gestodene or desogestrel. The warning, based on three unpublished epidemiological
+#' research studies, stated, “It is well known that the pill may rarely produce thrombosis (blood clots)
+#' involving veins of the legs. New evidence has become available indicating that the chance of thrombosis
+#' occurring in a vein increases about two-fold for some types of pills compared to others.
+#' Table 6.15 provides data from one of the studies.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.15
+#' @usage Table_6.15
+#' @format A data frame with four rows and four columns
+"Table_6.15"
+
+
+#' Chapter 6 Table_6.5
+#'
+#' In a retrospective study of lung cancer patients in 20 London hospitals,
+#' Richard Doll identified a relationship between smoking and lung cancer.
+#' Table 6.5 shows a partial set of data from his study. The data were
+#' collected on 60 female patients with lung cancer and 60 control females.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.5
+#' @usage Table_6.5
+#' @format A data frame with two rows and three columns
+"Table_6.5"
+
+
+#' Chapter 6 Table_6.7
+#'
+#' Your friend offers to play a simple game with you.
+#' He has a six-sided die and will roll it 30 times. Each time
+#' he rolls a 5 or 6, you will pay him $5. Each time he rolls
+#' a 1, 2, 3, or 4, he will pay you $5. The results of your
+#' friend’s 30 rolls for this game are shown in Table 6.7.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.7
+#' @usage Table_6.7
+#' @format A data frame with six rows and two columns
+"Table_6.7"
+
+
+#' Chapter 6 Table_6.9
+#'
+#' A study was conducted by introductory statistics students to see whether majors in the sciences are
+#' as likely to take a statistics course as majors in the humanities. They sampled 50 seniors from among
+#' humanities majors and another 50 seniors from among science majors and asked if they had taken a
+#' statistics course or were scheduled to take one before they graduated. Their data are provided in Table 6.9.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_6.9
+#' @usage Table_6.9
+#' @format A data frame with two rows and three columns
+"Table_6.9"
