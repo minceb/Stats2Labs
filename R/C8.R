9ee23962c713eb39d5b97a323132654855b45565
Bowen Mince <mincebow@grinnell.edu>
2021-01-25 10:26:05 -0600

    Chapter 8

---
+++ R/C8.R
@@ -0,0 +1,161 @@
+#' Chapter 8 APExams
+#'
+#' Are high-achieving high school students more inclined to take Advanced Placement exams? In a
+#' recent student survey, the co-author of this book asked students in his statistics courses to report the
+#' number of Advanced Placement (AP) exams they had taken in high school. In addition, information
+#' on their self-reported cumulative high school grade point average (HSGPA) and gender was recorded.
+#' Assume that students attended high school for four years.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name APExams
+#' @usage APExams
+#' @format A data frame with 100 rows and three columns
+"APExams"
+
+
+#' Chapter 8 CancerCluster
+#'
+#' Sample Cancer Data
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name CancerCluster
+#' @usage CancerCluster
+#' @format A data frame with eight rows and six columns
+"CancerCluster"
+
+
+#' Chapter 8 Gala
+#'
+#' Data on plant species on the Galápagos islands have been collected more than once. The most recent
+#' extensive data were reported by Johnson and Raven. We will consider two response variables: island total
+#' observed species count and island endemic species count. There are five potential covariates that we expect
+#' to be related to species count: island area (km2
+#' ) and elevation (meters), the distance (km) from the island to
+#' its nearest neighbor (adjacent island) and to the largest island (km2
+#' ) in the archipelago (Santa Cruz), and the
+#' area of the adjacent island (km2).
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Gala
+#' @usage Gala
+#' @format A data frame with 30 rows and eight columns
+"Gala"
+
+
+#' Chapter 8 Grandslam
+#'
+#' The data set Grandslam contains measurements on several commonly recorded
+#' statistics (e.g., numbers of grand slams, home runs, and runs batted) for 30 Major League Baseball teams. The
+#' values for the variables GrSlam (number of grand slams), Cycles (number of cycles), and Games (number
+#' of games) are count totals over five seasons from 2006 to 2010. The values for the remaining variables are
+#' averages over the five seasons.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Grandslam
+#' @usage Grandslam
+#' @format A data frame with 30 rows and 21 columns
+"Grandslam"
+
+
+#' Chapter 8 Hurricane
+#'
+#' With all the discussion about climate change in the past couple of decades, it is no surprise that the
+#' occurrence of several intense hurricanes over the past several years has garnered some attention. You
+#' may remember that in 2005 the U.S. Gulf coast was hit by hurricanes Emily, Katrina, and Rita. All three
+#' were category five on the Saffir-Simpson scale. That is the highest intensity on this five-level scale.
+#'
+#' The Data set hurricanes contains 59 lines, one for each year from 1950 through 2008. For each
+#' year, there are five columns of data containing counts of the number of hurricanes of each category
+#' type on the Saffir-Simpson scale (labeled cat1, cat2, cat3, cat4, or cat5). The data were
+#' obtained from http://weather.unisys.com/hurricane/atlantic
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Hurricane
+#' @usage Hurricane
+#' @format A data frame with 59 rows and six columns
+"Hurricane"
+
+
+#' Chapter 8 SkinCancer
+#'
+#' The data in Table 8.4 gives the incidence of non-melanoma skin cancer among women in
+#' Minneapolis–St. Paul by age group.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name SkinCancer
+#' @usage SkinCancer
+#' @format A data frame with 16 rows and five columns
+"SkinCancer"
+
+
+#' Chapter 8 SkinCancer_Alternative
+#'
+#' Alternative presentation of the SkinCancer Data
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name SkinCancer_Alternative
+#' @usage SkinCancer_Alternative
+#' @format A data frame with 16 rows and nine columns
+"SkinCancer_Alternative"
+
+
+#' Chapter 8 Smoking
+#'
+#' Several regular smokers were selected from a larger Minnesota survey about smoking habits. All reported
+#' smoking about the same number of cigarettes per day in recent months.
+#' Each was asked to report on the number of cigarettes smoked the day before (January 26) during the same 2-hour period. They were also asked
+#' whether they had been at work or at home or elsewhere during that 2-hour period.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Smoking
+#' @usage Smoking
+#' @format A data frame with one row and seven columns
+"Smoking"
+
+
+#' Chapter 8 Table_8.1
+#'
+#' Cancer cases and cancer incidence rates (CIR) by age group for the Bartlett–Green
+#' Acres neighborhood of Randolph (BGA), 1964–1984, and similar data from the
+#' Connecticut Tumor Registry (CTR), 1980–1984.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Table_8.1
+#' @usage Table_8.1
+#' @format A data frame with eight rows and six columns
+"Table_8.1"
+
+
+
+
+
+
+
+
