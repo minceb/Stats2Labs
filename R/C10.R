567fec5c67daf60da14f5f521b015c15a518c756
Bowen Mince <mincebow@grinnell.edu>
2021-01-27 09:03:05 -0600

    Chapter 10

---
+++ R/C10.R
@@ -0,0 +1,193 @@
+#' Chapter 10 CarsC10
+#'
+#' The data set Cars contains the make, model, equipment, mileage, and Kelley Blue Book suggested
+#' retail price of several used 2005 General Motor cars. Kelley Blue Book (http://www.kbb.com) has been
+#' an accurate resource for pricing cars for over 80 years and was used as a source to collect these data.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name CarsC10
+#' @usage CarsC10
+#' @format A data frame with 804 rows and 19 columns
+"CarsC10"
+
+
+#' Chapter 10 CrimeC10
+#'
+#' The FBI’s Uniform Crime Reporting (UCR) program gathers crime data from more than 17,000 law
+#' enforcement agencies throughout the United States. The Crime data set contains crime counts for
+#' several years in Iowa. Crimes are listed by type of crime (murder, burglary, larceny, etc.).
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name CrimeC10
+#' @usage CrimeC10
+#' @format A data frame with 47 rows and eight columns
+"CrimeC10"
+
+
+#' Chapter 10 GradesC10
+#'
+#' The data set Grades contains total scores for students’ homework, labs, quizzes, projects, and exams. This course had
+#' two 1-hour exams and a 3-hour final exam. In addition to a final project worth 20% of the final grade, there
+#' was an initial small project worth only 5% of the final grade. The exams were fairly standard statistics exams,
+#' but both projects required students to read peer-reviewed research projects, collect their own data, conduct an
+#' appropriate statistical analysis, and present their results. While several students in this introductory class had
+#' seen a little statistics in previous courses, this was typically the first time students were asked to read journal
+#' articles or conduct a realistic research project.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name GradesC10
+#' @usage GradesC10
+#' @format A data frame with 32 rows and 14 columns
+"GradesC10"
+
+
+#' Chapter 10 Grades_details
+#'
+#' Data supplementary to the GradesC10 data set.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Grades_details
+#' @usage Grades_details
+#' @format A data frame with 32 rows and 11 columns
+"Grades_details"
+
+
+#' Chapter 10 Intelligence
+#'
+#' The data set Intelligence contains test results for 214 people who have each taken several
+#' intelligence tests.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Intelligence
+#' @usage Intelligence
+#' @format A data frame with 214 rows and 10 columns
+"Intelligence"
+
+
+#' Chapter 10 Stock_2010
+#'
+#' The data set 2010Stock contains 2010 daily closing stock market values for several well-known
+#' companies.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Stock_2010
+#' @usage Stock_2010
+#' @format A data frame with 252 rows and 26 columns
+"Stock_2010"
+
+
+#' Chapter 10 Stock2_2006
+#'
+#' The stock market data set also contains dates for each business day in 2006.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Stock2_2006
+#' @usage Stock2_2006
+#' @format A data frame with 753 rows and six columns
+"Stock2_2006"
+
+
+#' Chapter 10 Stocks_2006
+#'
+#' The stock market data set also contains dates for each business day in 2006.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Stocks_2006
+#' @usage Stocks_2006
+#' @format A data frame with 251 rows and four columns
+"Stocks_2006"
+
+
+#' Chapter 10 TestC10
+#'
+#' The file Test contains several standardized variables.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name TestC10
+#' @usage TestC10
+#' @format A data frame with 113 rows and four columns
+"TestC10"
+
+
+#' Chapter 10 turtles
+#'
+#'  Jolicoeur and Mosimann measured the length, width, and height of 48 painted
+#'  turtle shells and conducted PCA on the data. Their work has been influential
+#'  in the field of allometry, the study of the
+#'  relative growth of a part of an organism in relation to an entire organism.
+#'
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name turtles
+#' @usage turtles
+#' @format A data frame with 48 rows and four columns
+"turtles"
+
+
+#' Chapter 10 VariableDescriptionsC10
+#'
+#'  A set of Chapter 10 data set descriptions.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name VariableDescriptionsC10
+#' @usage VariableDescriptionsC10
+#' @format A data frame with 114 rows and 16 columns
+"VariableDescriptionsC10"
+
+
+#' Chapter 10 Versicolor
+#'
+#'  The data set Versicolor contains four measurements (sepal length,
+#'  sepal width, petal length, and petal width) on 50 versicolor irises.
+#'  The petals are colorful and flashy,
+#'  while the sepal (under the petals) tends to be less colorful.
+#'
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Versicolor
+#' @usage Versicolor
+#' @format A data frame with 50 rows and four columns
+"Versicolor"
+
+
